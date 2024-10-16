03304ba52b9e589c0f22fac8420267ca215dfccb
Ameer Fakhri <kn_@hotmail.it>
2024-10-16 09:32:26 +0300

    Update CacheableQueryBuilder.php

--- src/Database/Query/CacheableQueryBuilder.php
+++ src/Database/Query/CacheableQueryBuilder.php
@@ -2,6 +2,7 @@
 
 namespace ElipZis\Cacheable\Database\Query;
 
+use Illuminate\Cache\RedisTaggedCache;
 use Illuminate\Cache\TaggableStore;
 use Illuminate\Database\Connection;
 use Illuminate\Database\Query\Builder;
@@ -137,10 +138,10 @@ class CacheableQueryBuilder extends Builder
         $modelClasses = $this->getIdentifiableModelClasses($this->getIdentifiableValue());
 
         // clear expired cached queries in tagged cache
-        if ($isTaggableStore) {
+        if ($isTaggableStore && Cache::tags($modelClasses) instanceof RedisTaggedCache) {
             Cache::tags($modelClasses)->flushStale();
         }
-        
+
         //If cached, return
         if (($isTaggableStore && Cache::tags($modelClasses)->has($cacheKey)) || Cache::has($cacheKey)) {
             $this->log("Found cache entry for '{$cacheKey}'");
